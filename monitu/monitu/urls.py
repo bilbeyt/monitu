e7d4f24916949fa44266519660e589000321e0d4
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2016-07-29 16:27:25 +0300

    Homepage configuration.

--- monitu/monitu/urls.py
+++ monitu/monitu/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from accounts import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.default.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
